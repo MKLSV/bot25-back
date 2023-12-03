d46aee5041c5011a7246f08dc7672478247ec997
Matvey Kolosov <118886359+MKLSV@users.noreply.github.com>
2023-12-03 17:26:47 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express()
-const PORT = process.env.PORT || 3001;
 
-// App config
-app.use(express.static('public'))
-app.use(express.json())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// LIST
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.listen(PORT, () => console.log('Server ready at port 3030!'))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
